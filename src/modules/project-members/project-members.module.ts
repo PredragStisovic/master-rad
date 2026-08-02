569592b890f116957a673ee5ab66600dc35fc892
Predrag Stisovic <predrag@Predrags-MacBook-Air.local>
2026-08-02 22:06:21 +0200

    Minor fixes

--- src/modules/project-members/project-members.module.ts
+++ src/modules/project-members/project-members.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { RolesModule } from '../roles/roles.module';
 import { ProjectMembersController } from './project-members.controller';
 import { ProjectMembersHelper } from './project-members.helper';
 import { ProjectMembersRepository } from './project-members.repository';
 import { ProjectMembersService } from './project-members.service';
 
 @Module({
+  imports: [RolesModule],
   controllers: [ProjectMembersController],
   providers: [
     ProjectMembersService,
--- src/modules/projects/projects.module.ts
+++ src/modules/projects/projects.module.ts
@@ -1,4 +1,5 @@
 import { Global, Module } from '@nestjs/common';
+import { RolesModule } from '../roles/roles.module';
 import { ProjectsController } from './projects.controller';
 import { ProjectsHelper } from './projects.helper';
 import { ProjectsRepository } from './projects.repository';
@@ -6,6 +7,7 @@ import { ProjectsService } from './projects.service';
 
 @Global()
 @Module({
+  imports: [RolesModule],
   controllers: [ProjectsController],
   providers: [ProjectsService, ProjectsHelper, ProjectsRepository],
   exports: [ProjectsService, ProjectsRepository],
