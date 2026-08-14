299c23d86bd48bee3f9874d972cc8787bc785719
Predrag Stisovic <stisovicpredrag@gmail.com>
2026-08-14 19:20:50 +0200

    Fixed migration issue

--- prisma/migrations/20260813095050_add_tsvector_columns/migration.sql
+++ prisma/migrations/20260813095050_add_tsvector_columns/migration.sql
@@ -1,21 +1,10 @@
-/*
-  Warnings:
-
-  - Added the required column `search` to the `projects` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `search` to the `tasks` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- AlterTable
-ALTER TABLE "projects" DROP COLUMN "search";
-
 ALTER TABLE "projects" ADD COLUMN "search" tsvector GENERATED ALWAYS AS (
   setweight(to_tsvector('english', name), 'A') ||
   setweight(to_tsvector('english', coalesce(description, '')), 'B')
 ) STORED;
 
 -- AlterTable
-ALTER TABLE "tasks" DROP COLUMN "search";
-
 ALTER TABLE "tasks" ADD COLUMN "search" tsvector GENERATED ALWAYS AS (
   setweight(to_tsvector('english', title), 'A') ||
   setweight(to_tsvector('english', coalesce(description, '')), 'B')
